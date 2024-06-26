a1368e8bb9993ebb3cdb5447f5b11f68ff4c40e7
s-gundorov <46057845+s-gundorov@users.noreply.github.com>
2024-06-26 13:43:51 +0300

    26-06-2024

---
+++ prog1/news/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class News_post(models.Model):
+    title = models.CharField('Название новости', max_length=50)
+    short_description = models.CharField('Краткое описание новости', max_length=200)
+    text = models.TextField('Новость')
+    pub_date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
---
+++ prog1/news/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from .models import News_post
+
+def home(request):
+	return render(request, 'news/news.html')
